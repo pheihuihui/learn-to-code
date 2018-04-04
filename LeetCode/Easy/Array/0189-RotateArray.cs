32c701f4b95c31904ba92c41ee03a2d382efce50
Phi <optimusgrass@live.com>
2018-04-04 23:02:11 +0800

    189

---
+++ LeetCode/Easy/Array/0189-RotateArray.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public void Rotate(int[] nums, int k)
+    {
+        int kk = k % nums.Length;
+        int[] tmp = new int[kk];
+        for(int i = nums.Length - kk; i < nums.Length; i++)
+        {
+            tmp[i - nums.Length + kk] = nums[i];
+        }
+        for(int i = nums.Length - 1; i >= kk; i--)
+        {
+            nums[i] = nums[i - kk];
+        }
+        for(int i = 0; i < kk; i++)
+        {
+            nums[i] = tmp[i];
+        }
+    }
+}
+
+//todo: find O(1) space solution
