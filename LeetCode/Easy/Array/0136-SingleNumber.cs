66a0f9c907c7873d3b634599ba596d5bef0b3109
Phi <optimusgrass@live.com>
2018-04-05 21:42:19 +0800

    136

---
+++ LeetCode/Easy/Array/0136-SingleNumber.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        int res = 0;
+        Dictionary<int, int?> dc = new Dictionary<int, int?>();
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(dc.GetValueOrDefault(nums[i]) == null)
+            {
+                dc.Add(nums[i], 1);
+            }
+            else
+            {
+                dc[nums[i]] += 1;
+            }
+        }
+        foreach(int s in dc.Keys)
+        {
+            if(dc[s].Value == 1)
+            {
+                res = s;
+            }
+        }
+        return res;
+    }
+}
