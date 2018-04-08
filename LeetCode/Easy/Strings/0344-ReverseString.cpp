141fac0c5a9ed3517db1b400915f432807aa6822
Phi <optimusgrass@live.com>
2018-04-08 23:52:08 +0800

    0344 cpp

---
+++ LeetCode/Easy/Strings/0344-ReverseString.cpp
@@ -0,0 +1,17 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	string reverseString(string s) {
+		int i = 0;
+		int j = s.length() - 1;
+		while (i < j)
+		{
+			swap(s[i], s[j]);
+			i++;
+			j--;
+		}
+		return s;
+	}
+};
