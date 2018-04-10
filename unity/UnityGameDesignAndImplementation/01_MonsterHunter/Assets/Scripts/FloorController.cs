8012b06964f06fdbf62618ade6f3ff6e6875917b
Phi <optimusgrass@live.com>
2018-04-10 23:26:54 +0800

    ...

---
+++ unity/UnityGameDesignAndImplementation/01_MonsterHunter/Assets/Scripts/FloorController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorController : MonoBehaviour
+{
+    public GameObject floorPrefab;
+    public Vector3 floorMoveSpeed;
+
+    private void Awake()
+    {
+        //Instantiate(floorPrefab);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(GenerateFloors());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        floorMoveSpeed += new Vector3(-0.01f, 0, 0);
+    }
+
+    IEnumerator GenerateFloors()
+    {
+        while (true)
+        {
+            GameObject gm = Instantiate(floorPrefab);
+            Rigidbody rd = gm.GetComponent<Rigidbody>();
+            rd.velocity = floorMoveSpeed;
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
