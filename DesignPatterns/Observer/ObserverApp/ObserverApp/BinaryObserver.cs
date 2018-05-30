2fb93db49034f921945fd661beb5c16eca2cb0d9
Fei <optimusgrass@live.com>
2018-05-30 22:09:19 +0800

    ...

---
+++ DesignPatterns/Observer/ObserverApp/ObserverApp/BinaryObserver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverApp
+{
+    public class BinaryObserver : Observer
+    {
+        public BinaryObserver(Subject subject)
+        {
+            this.subject = subject;
+            this.subject.Attach(this);
+        }
+
+        public override void Update()
+        {
+            Console.WriteLine("Binary: " + Convert.ToString(subject.state, 2));
+        }
+    }
+}
---
+++ DesignPatterns/Observer/ObserverApp/ObserverApp/OctalObserver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverApp
+{
+    public class OctalObserver : Observer
+    {
+        public OctalObserver(Subject subject)
+        {
+            this.subject = subject;
+            this.subject.Attach(this);
+        }
+
+        public override void Update()
+        {
+            Console.WriteLine("Binary: " + Convert.ToString(subject.state, 8));
+        }
+    }
+}
