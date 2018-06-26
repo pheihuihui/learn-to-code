6e4d426296131ff123a0dd3a88f6f1749ed59fa1
Fei <optimusgrass@live.com>
2018-06-26 23:12:07 +0800

    ...

--- DirectX/TutorialApp/dx12/dx/rendermain.cpp
+++ DirectX/TutorialApp/dx12/dx/rendermain.cpp
@@ -10,13 +10,11 @@ DWORD WINAPI RenderThreadMain(LPVOID lpThreadParameter) {
 
 #if defined(_DEBUG)
 	//debug layer
-	{
-		ID3D12Debug *pD3D12Debug;
-		if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&pD3D12Debug)))) {
-			pD3D12Debug->EnableDebugLayer();
-		}
-		pD3D12Debug->Release();
+	ID3D12Debug *pD3D12Debug;
+	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&pD3D12Debug)))) {
+		pD3D12Debug->EnableDebugLayer();
 	}
+	pD3D12Debug->Release();
 #endif
 
 
@@ -24,62 +22,55 @@ DWORD WINAPI RenderThreadMain(LPVOID lpThreadParameter) {
 	IDXGIFactory *pDXGIFactory;
 	CreateDXGIFactory(IID_PPV_ARGS(&pDXGIFactory));
 	ID3D12Device *pD3D12Device = NULL;
-	{
-		IDXGIAdapter *pDXGIAdapter;
-		for (UINT i = 0U; SUCCEEDED(pDXGIFactory->EnumAdapters(i, &pDXGIAdapter)); ++i) {
-			if (SUCCEEDED(D3D12CreateDevice(pDXGIAdapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&pD3D12Device)))) {
-				pDXGIAdapter->Release();
-				break;
-			}
+	IDXGIAdapter *pDXGIAdapter;
+	for (UINT i = 0U; SUCCEEDED(pDXGIFactory->EnumAdapters(i, &pDXGIAdapter)); ++i) {
+		if (SUCCEEDED(D3D12CreateDevice(pDXGIAdapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&pD3D12Device)))) {
 			pDXGIAdapter->Release();
+			break;
 		}
+		pDXGIAdapter->Release();
 	}
 
 	//create command queue
 	ID3D12CommandQueue *pDirectCommandQueue;
-	{
-		D3D12_COMMAND_QUEUE_DESC cqdc;
-		cqdc.Type = D3D12_COMMAND_LIST_TYPE::D3D12_COMMAND_LIST_TYPE_DIRECT;
-		cqdc.Priority = D3D12_COMMAND_QUEUE_PRIORITY::D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
-		cqdc.Flags = D3D12_COMMAND_QUEUE_FLAGS::D3D12_COMMAND_QUEUE_FLAG_NONE;
-		cqdc.NodeMask = 0X1;
-		pD3D12Device->CreateCommandQueue(&cqdc, IID_PPV_ARGS(&pDirectCommandQueue));
-	}
+	D3D12_COMMAND_QUEUE_DESC cqdc;
+	cqdc.Type = D3D12_COMMAND_LIST_TYPE::D3D12_COMMAND_LIST_TYPE_DIRECT;
+	cqdc.Priority = D3D12_COMMAND_QUEUE_PRIORITY::D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+	cqdc.Flags = D3D12_COMMAND_QUEUE_FLAGS::D3D12_COMMAND_QUEUE_FLAG_NONE;
+	cqdc.NodeMask = 0X1;
+	pD3D12Device->CreateCommandQueue(&cqdc, IID_PPV_ARGS(&pDirectCommandQueue));
+
 
 	//create swap chain
 	IDXGISwapChain *pDXGISwapChain;
-	{
-		DXGI_SWAP_CHAIN_DESC scdc;
-		scdc.BufferDesc.Width = 0U;
-		scdc.BufferDesc.Height = 0U;
-		scdc.BufferDesc.RefreshRate.Numerator = 60U;
-		scdc.BufferDesc.RefreshRate.Denominator = 1U;
-		scdc.BufferDesc.Format = DXGI_FORMAT::DXGI_FORMAT_R8G8B8A8_UNORM;
-		scdc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER::DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
-		scdc.BufferDesc.Scaling = DXGI_MODE_SCALING::DXGI_MODE_SCALING_UNSPECIFIED;
-		scdc.SampleDesc.Count = 1U;
-		scdc.SampleDesc.Quality = 0U;
-		scdc.BufferUsage = DXGI_USAGE_SHADER_INPUT;
-		scdc.BufferCount = 2;
-		scdc.OutputWindow = hWnd;
-		scdc.Windowed = TRUE;
-		scdc.SwapEffect = DXGI_SWAP_EFFECT::DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
-		scdc.Flags = 0U;
-		pDXGIFactory->CreateSwapChain(pDirectCommandQueue, &scdc, &pDXGISwapChain);
-	}
+	DXGI_SWAP_CHAIN_DESC scdc;
+	scdc.BufferDesc.Width = 0U;
+	scdc.BufferDesc.Height = 0U;
+	scdc.BufferDesc.RefreshRate.Numerator = 60U;
+	scdc.BufferDesc.RefreshRate.Denominator = 1U;
+	scdc.BufferDesc.Format = DXGI_FORMAT::DXGI_FORMAT_R8G8B8A8_UNORM;
+	scdc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER::DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	scdc.BufferDesc.Scaling = DXGI_MODE_SCALING::DXGI_MODE_SCALING_UNSPECIFIED;
+	scdc.SampleDesc.Count = 1U;
+	scdc.SampleDesc.Quality = 0U;
+	scdc.BufferUsage = DXGI_USAGE_SHADER_INPUT;
+	scdc.BufferCount = 2;
+	scdc.OutputWindow = hWnd;
+	scdc.Windowed = TRUE;
+	scdc.SwapEffect = DXGI_SWAP_EFFECT::DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
+	scdc.Flags = 0U;
+	pDXGIFactory->CreateSwapChain(pDirectCommandQueue, &scdc, &pDXGISwapChain);
 	pDXGIFactory->Release();
 
 
 	ID3D12DescriptorHeap *pRTVHeap;
-	{
-		D3D12_DESCRIPTOR_HEAP_DESC RTVHeapDesc = {
-			D3D12_DESCRIPTOR_HEAP_TYPE::D3D12_DESCRIPTOR_HEAP_TYPE_RTV, 
-			1,
-			D3D12_DESCRIPTOR_HEAP_FLAGS::D3D12_DESCRIPTOR_HEAP_FLAG_NONE,
-			0X1
-		};
-		pD3D12Device->CreateDescriptorHeap(&RTVHeapDesc, IID_PPV_ARGS(&pRTVHeap));
-	}
+	D3D12_DESCRIPTOR_HEAP_DESC RTVHeapDesc = {
+		D3D12_DESCRIPTOR_HEAP_TYPE::D3D12_DESCRIPTOR_HEAP_TYPE_RTV,
+		1,
+		D3D12_DESCRIPTOR_HEAP_FLAGS::D3D12_DESCRIPTOR_HEAP_FLAG_NONE,
+		0X1
+	};
+	pD3D12Device->CreateDescriptorHeap(&RTVHeapDesc, IID_PPV_ARGS(&pRTVHeap));
 	ID3D12Resource *pFrameBuffer;
 	pDXGISwapChain->GetBuffer(0, IID_PPV_ARGS(&pFrameBuffer));
 	pD3D12Device->CreateRenderTargetView(pFrameBuffer, NULL, pRTVHeap->GetCPUDescriptorHandleForHeapStart());
@@ -113,9 +104,21 @@ DWORD WINAPI RenderThreadMain(LPVOID lpThreadParameter) {
 	};
 	pDirectCommandList->ResourceBarrier(1, &RenderTargetToCommon);
 	pDirectCommandList->Close();
-	pDirectCommandQueue->ExecuteCommandLists(1, reinterpret_cast<ID3D12CommandList **>(&pDirectCommandList));
+	pDirectCommandQueue->ExecuteCommandLists(1, reinterpret_cast<ID3D12CommandList**>(&pDirectCommandList));
 
 
+	ID3D12RootSignature *pGRS;
+	HANDLE hGRSFile = CreateFileW(L"GRS.cso", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	LARGE_INTEGER szGRSFile;
+	GetFileSizeEx(hGRSFile, &szGRSFile);
+	HANDLE hGRSSection = CreateFileMappingW(hGRSFile, NULL, PAGE_READONLY, 0, szGRSFile.LowPart, NULL);
+	void *pGRSFile = MapViewOfFile(hGRSFile, FILE_MAP_READ, 0, 0, szGRSFile.LowPart);
+	pD3D12Device->CreateRootSignature(0X1, pGRSFile, szGRSFile.LowPart, IID_PPV_ARGS(&pGRS));
+	UnmapViewOfFile(pGRSFile);
+	CloseHandle(hGRSSection);
+	CloseHandle(hGRSFile);
+
+	pDirectCommandList->SetGraphicsRootSignature(pGRS);
 
 	pDXGISwapChain->Present(0, 0);
 
