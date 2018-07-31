8024beac6b05569b9929efc014c5577cd45890de
Fei <optimusgrass@live.com>
2018-07-31 18:31:16 +0800

    ...

---
+++ LeetCode/New/0342-Power-of-Four.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+	bool isPowerOfFour(int num) {
+		if (num == 0)
+			return false;
+		else {
+			while (num % 4 == 0)
+				num /= 4;
+			if (num == 1)
+				return true;
+			else
+				return false;
+		}
+	}
+};
